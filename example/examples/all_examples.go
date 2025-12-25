20bcbc79266e41bc86decb8e27c42ef10195ce5c
engmtcdrm <deedsoftheforsaken@gmail.com>
2025-12-25 16:29:39 -0500

    Updated examples

--- example/examples/all_examples.go
+++ example/examples/all_examples.go
@@ -1,9 +1,37 @@
 // Package examples provides a collection of examples for this project
 package examples
 
-type Example struct {
-	Name string
-	Fn   func()
+import (
+	"fmt"
+
+	"github.com/engmtcdrm/go-eggy"
+)
+
+var AllExamples = []eggy.Example{
+	{Name: "No Repeat Example", Fn: NoRepeatExample},
+	{Name: "Continue to Prompt Examples", Fn: RepeatPromptExample},
 }
 
-var AllExamples = []Example{}
+var promptExamples = []eggy.Example{
+	{Name: "Hello world!", Fn: HelloWorldExample},
+	{Name: "Hi Mom!", Fn: HiMomExample},
+}
+
+func HelloWorldExample() {
+	fmt.Println("Hello world!")
+}
+
+func HiMomExample() {
+	fmt.Println("Hi Mom!")
+}
+
+func NoRepeatExample() {
+	ex := eggy.NewExamplePrompt(promptExamples)
+	ex.Show()
+}
+
+func RepeatPromptExample() {
+	ex := eggy.NewExamplePrompt(promptExamples).
+		Repeat(true)
+	ex.Show()
+}
